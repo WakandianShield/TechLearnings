b359685dc50112543323df94eb8d9fba4f9e1904
WakandianShield <santiagogallardojr@gmail.com>
2026-08-21 08:18:46 -0600

    PW II - ANGULAR BASICS

---
+++ WebProgramming/PW II - ANGULAR BASICS (ACT 2)/routers/src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { About } from './components/about/about';
+
+export const routes: Routes = [{
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+}, {
+    path: 'home',
+    component: Home,
+},
+{
+    path: 'about',
+    component: About,
+}
+];
